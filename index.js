75e324af230a0c175f6190e0902319b165d2f294
kiranazka <160616395+kiranazka@users.noreply.github.com>
2024-05-26 01:01:14 +0500

    assignment

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-var tShirt = {
-    name: "T-Shirt",
-    price: 20,
-    colour: "Red",
-    inventory: {
-        stock: 20,
-        colorOptions: ["Red", "Blue", "Green"],
-        changeColour: function (newColour) {
-            this.colour = newColour;
-            if (newColour === "Red") {
-                this.price += this.price * 0.1;
-            }
-            else if (newColour === "Blue") {
-                this.price += this.price * 0.05;
-            }
-        },
-    },
+//Create a program that manages a simple friend list.
+// 1. Define an object named people containing an empty array called friends.
+// 2. Create three separate objects, each representing a friend, with properties like firstName,
+// lastName, and optionally id.
+// 3. Add these friend objects to the friends array within the people object.
+// 4. Output the entire people object to the console, displaying your information and your
+// friend list.
+// define type of parameter
+var people = {
+    friends: []
 };
-tShirt.inventory.changeColour("Blue");
-console.log(tShirt);
+var friend1 = {
+    firstName: "wardah",
+    lastName: "Naheed",
+    id: 101,
+    age: 20,
+    gender: "female",
+};
+var friend2 = {
+    firstName: "Ahmad",
+    lastName: "kamran",
+    id: 102,
+    age: 5,
+    gender: "male",
+};
+var friend3 = {
+    firstName: "Ayesha",
+    lastName: "khan",
+    id: 103,
+    age: 35,
+    gender: "female",
+};
+people.friends.push(friend1, friend2, friend3);
+console.log(people);
--- index.ts
+++ index.ts
@@ -1,35 +1,48 @@
-// type Colour
-type Colour = "Red" | "Blue" | "Green";
-// type product
-type Product = {
-    name: string,
-    price: number,
-    colour?: string,
-    inventory:{
-        stock: number,
-        colorOptions?: Colour[],
-        changeColour(newColour: Colour):
-        void;
-    };
+//Create a program that manages a simple friend list.
+// 1. Define an object named people containing an empty array called friends.
+// 2. Create three separate objects, each representing a friend, with properties like firstName,
+// lastName, and optionally id.
+// 3. Add these friend objects to the friends array within the people object.
+// 4. Output the entire people object to the console, displaying your information and your
+// friend list.
+// define type of parameter
+
+type Friend = {
+    firstName: string,
+    lastName: string,
+    id: number,
+    age: number,
+    gender: string,
+}
+let people = {
+    friends: [] as Friend[]
 };
-const tShirt: Product =
-{
-    name: "T-Shirt",
-    price: 20,
-    colour: "Red",
-    inventory: {
-        stock: 20,
-        colorOptions: ["Red", "Blue","Green"],
-        changeColour(newColour: Colour) {
-            this.colour = newColour;
-            if(newColour === "Red"){
-                this.price += this.price * 0.1;
-            }
-            else if(newColour === "Blue"){
-                this.price += this.price * 0.05;
-            }
-        },
-    },
-    };  
-tShirt.inventory.changeColour("Blue");
-console.log(tShirt);
+
+let friend1:Friend = {
+    firstName: "wardah",
+    lastName: "Naheed",
+    id: 101,
+    age: 20,
+    gender: "female",
+
+};
+let friend2:Friend = {
+    firstName: "Ahmad",
+    lastName: "kamran",
+    id: 102,
+    age: 5,
+    gender: "male",
+
+};
+let friend3:Friend = {
+    firstName: "Ayesha",
+    lastName: "khan",
+    id: 103,
+    age: 35,
+    gender: "female",
+
+};
+people.friends.push(friend1, friend2, friend3);
+console.log(people);
+
+ 
